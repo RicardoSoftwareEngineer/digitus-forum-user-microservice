766dde063d358a00c8033027892a7357f823d9d3
Ricardo Ribeiro <riceirolves@gmail.com>
2021-06-27 11:32:55 -0300

    findByDeletedIsFalse added to repository

--- src/main/java/com/digitusforum/user/model/repository/UserRepository.java
+++ src/main/java/com/digitusforum/user/model/repository/UserRepository.java
@@ -4,11 +4,13 @@ package com.digitusforum.user.model.repository;
 import com.digitusforum.user.model.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, String> {
     Optional<User> findByEmailAndPasswordAndDeletedIsFalse(String email, String password);
     Optional<User> findByEmailAndDeletedIsFalse(String email);
     Optional<User> findByUserIdAndDeletedIsFalse(String userId);
+    List<User> findByDeletedIsFalse();
     void deleteByUserId(String userId);
 }
