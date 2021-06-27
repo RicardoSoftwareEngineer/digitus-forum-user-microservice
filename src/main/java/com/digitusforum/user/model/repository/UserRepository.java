66e3f46c3e204140219ac1bd45de944ec81b9c20
Ricardo Ribeiro <riceirolves@gmail.com>
2021-06-26 21:08:00 -0300

    repository changed to be in compliance with user class

--- src/main/java/com/digitusforum/user/model/repository/UserRepository.java
+++ src/main/java/com/digitusforum/user/model/repository/UserRepository.java
@@ -7,10 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, String> {
-    Optional<User> findByLoginAndPasswordAndDeletedIsFalse(String email, String password);
     Optional<User> findByEmailAndPasswordAndDeletedIsFalse(String email, String password);
     Optional<User> findByEmailAndDeletedIsFalse(String email);
-    Optional<User> findByReadableIdAndDeletedIsFalse(Integer readableId);
     Optional<User> findByUserIdAndDeletedIsFalse(String userId);
     void deleteByUserId(String userId);
 }
